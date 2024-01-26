0d79716ffce6183d58128d37df16e984b9af735f
Varshini Chandrasekaran <varshiny04@gmail.com>
2024-01-26 21:06:24 +0530

    angular app and springboot app created

---
+++ otjservice/src/main/java/com/maantt/otj/otjservice/OtjserviceApplication.java
@@ -0,0 +1,13 @@
+package com.maantt.otj.otjservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtjserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtjserviceApplication.class, args);
+	}
+
+}
---
+++ otjservice/src/test/java/com/maantt/otj/otjservice/OtjserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maantt.otj.otjservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtjserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
