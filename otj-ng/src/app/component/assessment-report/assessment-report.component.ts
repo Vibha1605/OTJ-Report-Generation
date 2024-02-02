27d036c008b6f81ab3e808cd8578ce8b687d1a5d
Thamaraipriya <mthamaraipriyajma2514@gmail.com>
2024-02-02 23:16:56 +0530

    Bean Class Added

---
+++ otj-ng/src/app/component/assessment-report/assessment-report.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-assessment-report',
+  standalone: true,
+  imports: [],
+  templateUrl: './assessment-report.component.html',
+  styleUrl: './assessment-report.component.css'
+})
+export class AssessmentReportComponent {
+
+}
---
+++ otj-ng/src/app/model/assessment-report.spec.ts
@@ -0,0 +1,7 @@
+import { AssessmentReport } from './assessment-report';
+
+describe('AssessmentReport', () => {
+  it('should create an instance', () => {
+    expect(new AssessmentReport()).toBeTruthy();
+  });
+});
